c53129833f1d8aa58f37097bb8daaf74be7b931a
gilnorie <tomas.gilnoriega@webfleet.com>
2022-11-18 12:36:16 +0100

    remove unused import

--- src/main/java/com/thomas/realestate/RealEstateApplication.java
+++ src/main/java/com/thomas/realestate/RealEstateApplication.java
@@ -3,8 +3,6 @@ package com.thomas.realestate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-
 @SpringBootApplication(
 		scanBasePackages = {"com.thomas.realestate.infra.*"}
 )
